ed18832b3605c8f0e58c2320c0b19fa3ea9bccb1
Magaji Yuguda <musambaba56@gmail.com>
2020-10-31 03:34:27 +0100

    changes

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
           <a class="nav-link" href="#homePage" >Home <span class="sr-only">(current)</span></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#abouttUs">About Us</a>
+          <a class="nav-link" href="#aboutUs">About Us</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="rooms.php">Rooms</a>
